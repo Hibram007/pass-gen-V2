fd7637cc6380a5d86a9b3a77525b04f60a891304
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2022-11-29 13:21:05 -0700

    global varibles added to allow concatination --

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,37 @@
-// Assignment code here
+// --- Global variables ---//
+var length;
+var confirmNumber;
+var confirmSpecial;
+var confirmUpper;
+var confirmLower;
 
-// encapusulating Function to ask User if they'd like to make a pass word
+// Defining arrays that hold charscters user can choose from for Password//
+
+special = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+
+lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+number = [1,2,3,4,5,6,7,8,9];
+
+// Varible to house all user input choices
+var userAnswers;
+
+// // Event listner to launch createOrNot()  gen password function
+var generateBtn = document.querySelector("#generate");
+
+// // Event listner linked to HTML button.
+generateBtn.addEventListener("click", createOrNot);
+
+
+//--- ------------------INITIAL ASKING PROMPT TO CREATE PASS OR NOT ------------------------------//
+
+// Start Function to launch - Generation of Pass
 var createOrNot = function (){
 
 // prompt to ask player if they'd like to create Pass or not
-var promptPass = window.prompt(' would you like to "CREATE"  or "QUIT" a Password? ' );
+var promptPass = window.prompt(' would you like to "CREATE" a Password or "QUIT" ? ' );
 
 // Conditional 1 - if no input or incorrect input is given -- redirect to opening prompt
 if (promptPass === null || promptPass === "" ) {
@@ -24,43 +51,64 @@ if (confirmQuit) {
     return;
 }
 }
-// conditional 3 -- Series of prompts for pass criteria if user wants to CREATEf
+//---------------------------PASSWORD CRITERIA PROMPTS ----------------------------------------//
+
+// conditional 3 -- If Create is selected -- series of user prompts
 if (promptPass === 'CREATE' || promptPass === 'create' ) {
 
-// Charater length
-var passLength = window.prompt(" Type the number of of characters you would like the password to be. Type a number between 8 and 128. ");
+//ask user for --Character length
+var length = parseInt(window.prompt(" Type the number of of characters you would like the password to be. Type a number between 8 and 128. "));
 
-// parsed string from user input --- How to convert their "string #" to numeric data. 
-var userPassLength = parseInt(passLength);
-console.log(userPassLength);
+// input validation
+if(!length) {
+    alert("Insert correct numeric value between 8 and 128 ");
+// checks correct number range was selected
+} else if (length < 8 || length > 128) {
+    // restarts process
+    length = parseInt(window.prompt(" Type the number of of characters you would like the password to be. Type a number between 8 and 128. "));
 
-// Lowercase Characters?
- var lowerC = window.prompt(" Would you like to include Lowecase Characters? Type 'YES' or 'NO' . ");
+} else {
+    // validation of which character types will be used
 
- // Uppercase Characters?
- var upperC = window.prompt(" Would you like to include Uppercase Characters? Type 'YES' or 'NO' . ");
-
- // Numeric characters?
- // math random for random number gen
- var numberC = window.prompt("Would you like to include Numbers ? Type 'YES' or 'NO' . ");
-
- // Special Characters?
- var specialC = window.prompt("Would you like to include special characters ? Type 'YES' or 'NO' .  ");
+    confirmSpecial = confirm("Will this contain Special characters? ");
+    confirmLower = confirm("Will this contain Lowercase letters? ");
+    confirmUpper = confirm("Will this contain Uppercase letters? ");
+    confirmNumber = confirm("Will this contain numbers? ");
+};
 }
+
+//------------- CONDITIONALS BASED ON USER INPUT AND FOR LOOP  TO CREATE PASSWORD FROM IT -------/////
+
+// IF ---- 8 - 128 ----Then make pass this # length--- (RESTRICT PASSWORD LENGTH --- MIN AND MAX CONDITIONS/ METHODS)
+
+// IF ---- If YES to LowerC --  Include
+// ---- Else --- NO to LowerC --- Then do not include and move on
+
+// IF ---- If YES to UpperC -- Include
+// ---- Else --- NO to UpperC --- Then do not include and move on
+
+// IF ---- If YES to numberC -- Include
+// ---- Else --- NO to numberC --- Then do not include and move on
+
+// IF ---- If YES to specialC -- Include
+// ---- Else --- NO to specialC --- Then do not include and move on
+
+// Takes Results and compiles a Password//
+
+// FOR LOOP THAT LOOPS THROUGH AND CREATES PASSWORD BASED ON WHAT CONDITIONS WE SET AS TRUE// 
+
+// Password is displayed in an alert or Text Box!
+
+//-------------------------------- Prompt asking if another password is desired -------------------------//
+
+// IF --- YES restart CreateOrNot 
+//     ---- NO --- 
 
 
 // exits promptpass function
-return;
-};
+// return;
+// };
 
-
-
-// Event listern goes at the very end
-// // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// // Add event listener to generate button
-generateBtn.addEventListener("click", createOrNot);
 
 // OG code use if needed 
 // // Write password to the #password input
@@ -74,4 +122,5 @@ generateBtn.addEventListener("click", createOrNot);
 // //   passwordText.value = password;
 // console.log( " I touched the button");
 // }
-// passGen();
+}
+createOrNot();
